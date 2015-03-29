7b85d35ea6827dd5a351d5c51722111d6545ae68
hogejiro <1515179+hogejiro@users.noreply.github.com>
2015-03-29 23:55:31 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "source ~/dotfiles/.vim/.vimrc" >> ~/.vimrc
 echo "source ~/dotfiles/.zsh.d/.zshrc" >> ~/.zshrc
+echo "source-file ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/.zsh.d/.oh-my-zsh
 chsh -s /bin/zsh
 mkdir ~/dotfiles/.vim/bundle
