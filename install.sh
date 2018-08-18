918ef2b0f5f5e5374f7f60801820e398a6176959
hogejiro <combatechizen2@gmail.com>
2018-08-19 00:15:39 +0900

    modify .tmux.conf for tmux version >= 2.1 & modify a little bit

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ echo "source ~/dotfiles/.vim/.vimrc" >> ~/.vimrc
 echo "source ~/dotfiles/.zsh.d/.zshrc" >> ~/.zshrc
 echo "source-file ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/.zsh.d/.oh-my-zsh
-chsh -s /bin/zsh
-mkdir ~/dotfiles/.vim/bundle
+# chsh -s /bin/zsh
+mkdir -p ~/dotfiles/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim.git ~/dotfiles/.vim/bundle/neobundle.vim
 mkdir -p ~/tmp/vim
-cd ~/dotfiles/.vim/bundle/vimproc/
-make
+# cd ~/dotfiles/.vim/bundle/vimproc/
+# make
