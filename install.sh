43970247b0be82aea140e589d02d85a0189eaffe
hogejiro <1515179+hogejiro@users.noreply.github.com>
2021-02-14 01:52:32 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ echo "source ~/dotfiles/.zsh.d/.zshrc" >> ~/.zshrc
 echo "source-file ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/.zsh.d/.oh-my-zsh
 # chsh -s /bin/zsh
-mkdir -p ~/dotfiles/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim.git ~/dotfiles/.vim/bundle/neobundle.vim
+mkdir -p ~/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git ~/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim
 mkdir -p ~/tmp/vim
-# cd ~/dotfiles/.vim/bundle/vimproc/
-# make
