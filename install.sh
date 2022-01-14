1d0302485dc0c09aa4105491d362d50fa414d7c0
hogejiro <1515179+hogejiro@users.noreply.github.com>
2022-01-15 00:11:03 +0900

    add description for jump

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/.zsh.d/.oh-my-z
 mkdir -p ~/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim
 mkdir -p ~/tmp/vim
+# install jump https://github.com/gsamokovarov/jump/
